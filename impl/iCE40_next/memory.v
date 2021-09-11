e83fb2a89a22abf7bb6202b256c393c4211a5614
James Sharp <james.sharp@csiro.au>
2021-09-11 19:28:48 +1000

    Added scaffolding for iCE40_next impl

---
+++ impl/iCE40_next/memory.v
@@ -0,0 +1,49 @@
+/* memory.v : Memory */
+
+module memory
+#(parameter BITS = 16, ADDRESS_BITS = 15)
+(
+	input CLK,
+	input [ADDRESS_BITS - 1 : 0]  ADDRESS,
+	input [BITS - 1 : 0] DATA_IN,
+	output [BITS - 1 : 0] DATA_OUT,
+	input WRb  /* write memory */  
+);
+
+wire [15:0] data_out1;
+wire [15:0] data_out2;
+
+assign DATA_OUT = ((ADDRESS[14] == 1'b0) ? data_out1 : data_out2);
+
+wire WEN1 = (ADDRESS[14] == 1'b0) ? ((WRb == 1'b0) ? 1'b1 : 1'b0) : 1'b0;
+wire WEN2 = (ADDRESS[14] == 1'b1) ? ((WRb == 1'b0) ? 1'b1 : 1'b0) : 1'b0;
+
+SB_SPRAM256KA spram0
+(
+.ADDRESS(ADDRESS[13:0]),
+.DATAIN(DATA_IN),
+.MASKWREN(4'b1111),
+.WREN(WEN1),
+.CHIPSELECT(1'b1),
+.CLOCK(CLK),
+.STANDBY(1'b0),
+.SLEEP(1'b0),
+.POWEROFF(1'b1),
+.DATAOUT(data_out1)
+);
+
+SB_SPRAM256KA spram1
+(
+.ADDRESS(ADDRESS[13:0]),
+.DATAIN(DATA_IN),
+.MASKWREN(4'b1111),
+.WREN(WEN2),
+.CHIPSELECT(1'b1),
+.CLOCK(CLK),
+.STANDBY(1'b0),
+.SLEEP(1'b0),
+.POWEROFF(1'b1),
+.DATAOUT(data_out2)
+);
+
+endmodule
---
+++ impl/iCE40_next/slurm16_ice40.v
@@ -0,0 +1,56 @@
+module top
+(
+	output UART_TX,
+
+	output GPIO1,
+	output GPIO2,
+
+	output RED_LED,
+	output GREEN_LED,
+	output BLUE_LED,
+	
+);
+
+wire clk;
+
+localparam CLOCKFREQ = 12000000;
+
+SB_HFOSC inthosc (
+  .CLKHFPU(1'b1),
+  .CLKHFEN(1'b1),
+  .CLKHF(clk)
+);
+defparam inthosc.CLKHF_DIV = "0b10";
+
+
+reg [20:0] COUNT = 0;
+wire RSTb = (COUNT < 10000) ? 1'b0 : 1'b1;
+
+always @(posedge clk)
+begin
+	if (COUNT < 100000)
+		COUNT <= COUNT + 1;	
+end
+
+wire [15:0] PINS;
+
+assign UART_TX = PINS[15];
+assign GPIO1 = PINS[0];
+assign GPIO2 = PINS[1];
+assign RED_LED = PINS[8];
+assign GREEN_LED = PINS[9];
+assign BLUE_LED = PINS[10];
+
+
+
+slurm16 #(
+	.CLOCK_FREQ(CLOCKFREQ)
+) slm0 (
+	clk,
+	RSTb,
+	PINS
+);
+
+
+
+endmodule
