c5f2f5354c16cf3690b9678071c35a4871f81cfa
James Sharp <james.sharp@csiro.au>
2021-10-04 20:51:03 +1100

    Added wrapper for hardened spi ip, seems to work (can read back written ip register ok)

--- impl/iCE40/Test.asm
+++ impl/iCE40/Test.asm
@@ -1,187 +1,53 @@
         .org 0h
 
+SPI_FLASH_BASE 		equ	0x1400
+SPI_FLASH_ADDR 		equ (SPI_FLASH_BASE + 0)
+SPI_FLASH_DATA_OUT 	equ (SPI_FLASH_BASE + 1)
+SPI_FLASH_DATA_IN	equ (SPI_FLASH_BASE + 2)
+SPI_FLASH_CMD		equ (SPI_FLASH_BASE + 3)
+SPI_FLASH_STATUS	equ (SPI_FLASH_BASE + 4)
+
 UART_TX_REG 	equ 	0x1000
-UART_TX_STATUS  equ		0x1001
-PWM_LED			equ 	0x1200
 
-HIRAM			equ 	0x2000
-UPPER_HIRAM		equ		0x8000
+// Registers for SPI core
 
-GFX_SPRITE_FRAME	equ 0x2012
-GFX_SPRITE_X		equ 0x2010
-GFX_SPRITE_Y		equ 0x2011
-GFX_SPRITE_FLIP		equ 0x2013
-GFX_FRAME			equ 0x2000
+SPICR0 equ 0x8  //1000 
+SPICR1 equ 0x9  //1001 
+SPICR2 equ 0xa  //1010 
+SPIBR  equ 0xb  //1011 
+SPITXDR equ 0xd //1101 
+SPIRXDR equ 0xe //1110 
+SPICSR equ 0xf  //1111 
+SPISR  equ 0xc  //1100 
+SPIIRQ equ 0x6  //0110 
 
-GPIO_IN				equ 0x1101
+	nop
+	
+	mov r0, SPICR0
+	st [SPI_FLASH_ADDR], r0
+	mov r0, 2
+	st [SPI_FLASH_DATA_IN], r0
+	mov r0, 1
+	st [SPI_FLASH_CMD], r0
+wait:	
+ 	ld r0, [SPI_FLASH_STATUS]
+	test r0, 1
+	bz wait
 
-UP_BUTTON			equ 1<<0
-DOWN_BUTTON			equ 1<<1
-LEFT_BUTTON			equ 1<<2
-RIGHT_BUTTON		equ 1<<3
-A_BUTTON			equ 1<<4
-B_BUTTON			equ 1<<5
+	mov r0, 2
+	st [SPI_FLASH_CMD], r0
 
-SPRITE_NORMAL		equ 0
-SPRITE_90_DEG		equ 2
-SPRITE_180_DEG		equ 1
-SPRITE_270_DEG		equ 3
+wait2:	
+ 	ld r0, [SPI_FLASH_STATUS]
+	test r0, 2
+	bz wait2
 
-AUDIO_PHASE			equ 0x1300
-AUDIO_AMPL			equ 0x1301
-
-		nop
-		
-run:
-
-		mov r0, the_string
-loop:
-		ld r1, [r0]
-		inc r0
-		or r1, r1
-		bz die 
-		st [UART_TX_REG], r1
-test_loop:
-		ld r2, [UART_TX_STATUS]
-		test r2, 1
-		bz test_loop
-
-		ba loop
+	ld r0, [SPI_FLASH_DATA_OUT]
+	add r0, 'A'
+	st [UART_TX_REG], r0
 
 die:
-		// r0 = sprite frame
-		// r1 = sprite x
-		// r2 = sprite y
-		// r3 = frame number
-		// r5 = state
-		// r7 = audio freq
-
-		mov r5, 0
-		mov r0, 0
-		mov r1, 320 + 48
-		mov r2, 240 + 33
-		ld r3, [GFX_FRAME]		
-
-		mov r7, 0x7fff
-		st [AUDIO_AMPL], r7
-
-gfx_loop:
-		// Check buttons
-	
-		ld r6, [GPIO_IN]
-		cmp r6, UP_BUTTON
-		bnz  not_up
-		
-		// Up pressed
-
-		mov r5, SPRITE_270_DEG
-		sub r2, 1
-		add r0, 1
-
-		or r7, r7
-		bnz done_button		
-
-		mov r7, 1000
-
-		ba done_button
-
-not_up:
-		cmp r6, DOWN_BUTTON
-		bnz not_down	
-
-		// Down pressed
-		
-		mov r5, SPRITE_90_DEG
-		add r2, 1
-		add r0, 1
-
-		or r7, r7
-		bnz done_button		
-
-		mov r7, 1000
-
-		ba done_button
-
-not_down:
-
-		cmp r6, LEFT_BUTTON
-		bnz not_left
-
-		// Left pressed
-		
-		mov r5, SPRITE_180_DEG
-		sub r1, 1
-		add r0, 1
-
-		or r7, r7
-		bnz done_button		
-
-		mov r7, 1000
-
-		ba done_button
-		
-not_left:
-
-		cmp r6, RIGHT_BUTTON
-		bnz not_right
-
-		// Right pressed
-		
-		mov r5, SPRITE_NORMAL
-		add r1, 1
-		add r0, 1
-
-		or r7, r7
-		bnz done_button		
-
-		mov r7, 1000
-
-		ba done_button
-	
-not_right:
-		mov r0, 0
-		mov r7, 0
+	ba die
 
 
-done_button:
-		nop
-
-		cmp r0, 12
-		bnz no_trunc
-		mov r0, 0
-no_trunc:
-
-		st  [GFX_SPRITE_FLIP], r5
-
-		mov r4, r0
-		lsr r4
-		lsr r4
-		st [GFX_SPRITE_FRAME], r4
-		st [GFX_SPRITE_X], r1
-		st [GFX_SPRITE_Y], r2
-
-	    st [AUDIO_PHASE], r7
-		or r7,r7
-		bz wait_frame
-		
-		sub r7, 100
-
-		// Wait for retrace
-
-wait_frame:
-		ld r4, [GFX_FRAME]
-		cmp r4, r3
-		bz wait_frame
-		mov r3, r4
-
-done_state_3:
-		ba gfx_loop
-
-		dw 0
-the_string:
-		dw "SLURM16 Sprite test\r\n"
-		dw 0
-
-		.end
-
-
+	.end
--- impl/iCE40/spi_flash.v
+++ impl/iCE40/spi_flash.v
@@ -19,8 +19,11 @@ module spi_flash
 	input MISO
 );
 
-
-
+localparam idle = 3'b000;
+localparam read = 3'b001;
+localparam read_done  = 3'b010;
+localparam write 	  = 3'b011;
+localparam write_done = 3'b100;
 
 reg [7:0] address_r;
 reg [7:0] address_r_next;
@@ -28,27 +31,91 @@ reg [7:0] address_r_next;
 reg [7:0] data_in_r;
 reg [7:0] data_in_r_next;
 
-reg [7:0] cmd_r;
-reg [7:0] cmd_r_next;
-
-reg [7:0] data_out_r = 8'h00;
-
-reg [7:0] status_r = 8'h00;
-
 reg [BITS - 1 : 0] dout_r;
 
+reg [2:0] state_r = idle;
+reg [2:0] state_r_next;
+
+reg go_read_r;
+reg go_read_r_next;
+
+reg go_write_r;
+reg go_write_r_next;
+
 assign DATA_OUT = dout_r;
+
+reg spi_stb;
+reg spi_rw;
+
+wire spi_ack;
+
+wire [7:0] spi_data_out;
+
+reg [7:0] spi_data_out_r;
+reg [7:0] spi_data_out_r_next;
+
+reg read_done_r, read_done_r_next;
+reg write_done_r, write_done_r_next;
+
+SB_SPI SB_SPI_inst( .SBCLKI(CLK), 
+					.SBSTBI(spi_stb), 
+					.SBRWI(spi_rw),
+					/* address bus */
+      				.SBADRI0(address_r[0]), 
+					.SBADRI1(address_r[1]), 
+					.SBADRI2(address_r[2]), 
+					.SBADRI3(address_r[3]), 
+					.SBADRI4(address_r[4]), 
+					.SBADRI5(address_r[5]), 
+					.SBADRI6(address_r[6]), 
+					.SBADRI7(address_r[7]),
+					/* data in */
+      				.SBDATI0(data_in_r[0]), 
+					.SBDATI1(data_in_r[1]), 
+					.SBDATI2(data_in_r[2]), 
+					.SBDATI3(data_in_r[3]), 
+					.SBDATI4(data_in_r[4]), 
+					.SBDATI5(data_in_r[5]), 
+					.SBDATI6(data_in_r[6]), 
+					.SBDATI7(data_in_r[7]),
+      				/* data out */
+					.SBDATO0(spi_data_out[0]), 
+					.SBDATO1(spi_data_out[1]), 
+					.SBDATO2(spi_data_out[2]), 
+					.SBDATO3(spi_data_out[3]), 
+					.SBDATO4(spi_data_out[4]), 
+					.SBDATO5(spi_data_out[5]), 
+					.SBDATO6(spi_data_out[6]), 
+					.SBDATO7(spi_data_out[7]),
+      				.SBACKO(spi_ack),
+      				.MI(MISO), 
+      				.MO(MOSI), 
+					.SCKO(SCK),
+					.MCSNO0(SSb)
+   );
+
 
 always @(posedge CLK)
 begin
 	if (RSTb == 1'b0) begin
 		address_r  <= 8'h00;
 		data_in_r <= 8'h00;
-		status_r   <= 8'h00;
+		state_r <= idle;
+		go_read_r <= 1'b0;
+		go_write_r <= 1'b0;
+		spi_data_out_r <= 8'h00;
+		read_done_r <= 1'b0;
+		write_done_r <= 1'b0;
 	end else begin
 		address_r  <= address_r_next;
 		data_in_r  <= data_in_r_next;
-		cmd_r      <= cmd_r_next;
+		state_r    <= state_r_next;
+		go_read_r  <= go_read_r_next;
+		go_write_r <= go_write_r_next;
+		spi_data_out_r <= spi_data_out_r_next;
+		read_done_r <= read_done_r_next;
+		write_done_r <= write_done_r_next;
+	
 	end
 end
 
@@ -57,9 +124,11 @@ begin
 
 	address_r_next = address_r;
 	data_in_r_next = data_in_r;
-	cmd_r_next = cmd_r;
 
 	dout_r = {BITS{1'b0}};
+
+	go_write_r_next = 1'b0;
+	go_read_r_next = 1'b0;
 
 	case (ADDRESS)
 		8'h00:	/* Address reg */
@@ -67,18 +136,72 @@ begin
 				address_r_next = DATA_IN[7:0];
 			end
 		8'h01: /* Data out reg */
-			dout_r = data_out_r;
+			dout_r = spi_data_out_r;
 		8'h02: /* Data in reg */
 			if (WR == 1'b1) begin
 				data_in_r_next = DATA_IN[7:0];
 			end
 		8'h03: /* Command bits reg */
 			if (WR == 1'b1) begin
-				cmd_r_next = DATA_IN[7:0];
+				go_write_r_next = DATA_IN[0];
+				go_read_r_next  = DATA_IN[1];
 			end
 		8'h04: /* Status bits reg */
-			dout_r = status_r;
+			dout_r = {6'b000000, read_done_r, write_done_r};
 		default:;
+	endcase
+end
+
+// Glue logic state machine to talk to IP
+
+always @(*)
+begin
+	state_r_next = state_r;	
+	spi_data_out_r_next = spi_data_out_r;
+	read_done_r_next = read_done_r;
+	write_done_r_next = write_done_r;
+
+	spi_stb = 1'b0;
+	spi_rw = 1'b0;
+
+	case (state_r)
+		idle:
+			if (go_write_r == 1'b1) begin
+				state_r_next 		= write;
+				write_done_r_next 	= 1'b0;
+			end
+			else if (go_read_r == 1'b1) begin
+				state_r_next 		= read;
+				read_done_r_next 	= 1'b0;
+			end
+		read: begin
+			spi_stb = 1'b1;
+			spi_rw = 1'b0;
+
+			if (spi_ack == 1'b1) begin
+				state_r_next 		= read_done;
+				spi_data_out_r_next = spi_data_out;  
+			end
+
+		end
+		read_done: begin
+			read_done_r_next = 1'b1;
+			state_r_next = idle;
+		end
+		write: begin
+			spi_stb = 1'b1;
+			spi_rw = 1'b1;
+
+			if (spi_ack == 1'b1) begin
+				state_r_next 		= write_done;
+			end
+		end
+		write_done: begin
+			write_done_r_next = 1'b1;
+			state_r_next = idle;
+		end
+		default: ;
+
 	endcase
 end
 
